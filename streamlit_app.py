397a922dbb87f244fbd2cf7c3c7b5a7311bf5510
Cristina Cigarran <82393916+ccdiaz@users.noreply.github.com>
2022-12-14 12:51:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Chocolate Chip Pancakes')
+streamlit.text('Poffertjes with Lotus butter')
