b192e8bdc1aa2480c5dec6be714254ef205ea64f
Cristina Cigarran <82393916+ccdiaz@users.noreply.github.com>
2022-12-14 12:52:59 +0100

    Update streamlit_app.py
    added breakfast menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Chocolate Chip Pancakes')
-streamlit.text('Poffertjes with Lotus butter')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
